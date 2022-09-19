4db5eaebd64a5fa6bf7e06b4bbd283b58d55688a
humberto.dutra@hotmail.com <humberto.dutra@hotmail.com>
2022-09-19 14:09:53 -0300

    exer1

---
+++ Programming-Orient-Obeject-Poo-Solid/dayOne/students.class.ts
@@ -0,0 +1,23 @@
+export default class students{
+    private _registration: string;
+    private _name: string;
+    private _gradeOne: number;
+    private _gradeTwo: number;
+    private _gradeThree: number;
+    private _gradeFour: number;
+    private _gradeJobOne: number;
+    private _gradeJobTwo: number;
+
+    constructor(
+        registration: string,
+        name: string,
+        gradeOne: number,
+        gradeTwo: number,
+        gradeThree: number,
+        gradeFour: number,
+        gradeJobOne: number,
+        gradeJobTwo: number,
+    ) {
+
+    }
+}
