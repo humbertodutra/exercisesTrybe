5be0143d5b4276fa325dd4dd2873994aa8cf1fbe
humberto.dutra@hotmail.com <humberto.dutra@hotmail.com>
2022-09-19 14:32:59 -0300

    correcting exercise one

--- Programming-Orient-Obeject-Poo-Solid/dayOne/students.class.ts
+++ Programming-Orient-Obeject-Poo-Solid/dayOne/students.class.ts
@@ -1,23 +1,44 @@
-export default class students{
+export default class Student{
     private _registration: string;
     private _name: string;
-    private _gradeOne: number;
-    private _gradeTwo: number;
-    private _gradeThree: number;
-    private _gradeFour: number;
-    private _gradeJobOne: number;
-    private _gradeJobTwo: number;
+    private _examGrades: number[];
+    private _workGrades: number[];
+   
 
     constructor(
         registration: string,
         name: string,
-        gradeOne: number,
-        gradeTwo: number,
-        gradeThree: number,
-        gradeFour: number,
-        gradeJobOne: number,
-        gradeJobTwo: number,
+    
+       
     ) {
+        this._registration = registration;
+        this._name = name;
+        this._examGrades = [];
+        this._workGrades = [];
 
+}
+
+    set examGrades(value: number[]){
+        if (value.length !== 4){
+            throw new Error('The student must have 4 grades ')
+        }
+        this._examGrades = value;
+    }
+
+    set workGrades(value: number[]){
+        if (value.length !== 2){
+            throw new Error('The student must have 2 work grades')
+        }
+        this._workGrades = value;
     }
 }
+
+
+const personOne = new Student('202001011', 'Ronaldinho Gaucho');
+const studentOneGrades = [10,9,6,4];
+const studentOneWorkGrades = [5,9];
+
+personOne.examGrades = studentOneGrades;
+personOne.workGrades = studentOneWorkGrades;
+
+console.log(personOne);
